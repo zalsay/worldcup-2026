9e003ebe10d322c9a0455aaf86bf1f467a3da3e7
zalsay <51143152+zalsay@users.noreply.github.com>
2026-06-24 09:51:15 +0800

    chore: 合并续补预测数据

---
+++ frontend/src/data.js
@@ -0,0 +1,16 @@
+import { matches as baseMatches, reportMeta as baseReportMeta, standings } from "./matches.js";
+import { extraMatches } from "./extra-matches.js";
+
+export const reportMeta = {
+  ...baseReportMeta,
+  generatedAt: "2026-06-24 00:00 UTC",
+  dateLabel: "2026-06-24"
+};
+
+export { standings };
+
+export const matches = [...baseMatches, ...extraMatches];
+
+export function findMatch(matchId) {
+  return matches.find((match) => match.id === matchId);
+}
